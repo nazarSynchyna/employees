d4bc03b5ec2924b60b66a8595fafdcacf64b3206
Nazar Synchyna <nsynchyna@gmail.com>
2026-08-05 15:54:44 +0300

    feat: add SearchPanel component

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,12 +1,18 @@
-import './App.css';
-import AppInfo from '../app-info/app-info';
+import AppInfo from "../app-info/app-info";
+import SearchPanel from "../search-panel/search-panel";
+
+import "./App.css";
 
 function App() {
   return (
     <div className="app">
       <AppInfo />
+
+      <div className="search-panel">
+        <SearchPanel />
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
