7e75ab8f353448a2062aba9b4e062e846509d3da
Nazar Synchyna <nsynchyna@gmail.com>
2026-08-06 16:13:39 +0300

    feat(EmployeesListItem): add rise state toggle on name click

--- src/components/empoyees-list-item/employees-list-item.jsx
+++ src/components/empoyees-list-item/employees-list-item.jsx
@@ -6,27 +6,39 @@ class EmployeesListItem extends Component {
     super(props);
     this.state = {
       increase: false,
+      rise: false,
     };
   }
 
   onIncrease = () => {
-    this.setState(({increase}) => ({
-      increase: !increase
-    }))
-  }
+    this.setState(({ increase }) => ({
+      increase: !increase,
+    }));
+  };
+
+  onRise = () => {
+    this.setState(({ rise }) => ({
+      rise: !rise,
+    }));
+  };
 
   render() {
-    const { name, salary} = this.props;
-    const {increase} = this.state;
+    const { name, salary } = this.props;
+    const { increase, rise } = this.state;
 
     let classNames = "list-group-item d-flex justify-content-between";
     if (increase) {
       classNames += " increase";
     }
+    if (rise) {
+      classNames += " like";
+    }
 
     return (
       <li className={classNames}>
-        <span className="list-group-item-label">{name}</span>
+        <span className="list-group-item-label" onClick={this.onRise}>
+          {name}
+        </span>
         <input
           type="text"
           className="list-group-item-input"
