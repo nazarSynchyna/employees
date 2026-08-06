5c1631b2705c45828ebf62c9c32aa25cfce32fc3
Nazar Synchyna <nsynchyna@gmail.com>
2026-08-06 16:15:40 +0300

    refactor(EmployeesAddForm): convert to class component

--- src/components/empoyees-add-form/employees-add-form.jsx
+++ src/components/empoyees-add-form/employees-add-form.jsx
@@ -1,27 +1,30 @@
+import { Component } from "react";
 import "./employees-add-form.css";
 
-const EmployeesAddForm = () => {
-  return (
-    <div className="app-add-form">
-      <h3>Додати нового співробітника</h3>
-      <form className="add-form d-flex">
-        <input
-          type="text"
-          className="form-control new-post-label"
-          placeholder="Як його звуть?"
-        />
-        <input
-          type="number"
-          className="form-control new-post-label"
-          placeholder="З/П в $?"
-        />
+class EmployeesAddForm extends Component {
+  render() {
+    return (
+      <div className="app-add-form">
+        <h3>Додати нового співробітника</h3>
+        <form className="add-form d-flex">
+          <input
+            type="text"
+            className="form-control new-post-label"
+            placeholder="Як його звуть?"
+          />
+          <input
+            type="number"
+            className="form-control new-post-label"
+            placeholder="З/П в $?"
+          />
 
-        <button type="submit" className="btn btn-outline-light">
-          Додати
-        </button>
-      </form>
-    </div>
-  );
-};
+          <button type="submit" className="btn btn-outline-light">
+            Додати
+          </button>
+        </form>
+      </div>
+    );
+  }
+}
 
 export default EmployeesAddForm;
