2fc5bbb2868c1eb519a19c030e1aa164bdc9dbdc
Nazar Synchyna <nsynchyna@gmail.com>
2026-08-07 14:43:36 +0300

    feat(search): implement employee live search functionality

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -17,6 +17,7 @@ class App extends Component {
         { id: 2, name: "Alex M.", salary: 3000, increase: true, rise: false },
         { id: 3, name: "Carl W.", salary: 5000, increase: true, rise: false },
       ],
+      term: ''
     };
     this.maxId = 4;
   }
@@ -56,19 +57,33 @@ class App extends Component {
     }));
   };
 
+  searchEmp = (items, term) => {
+    if (term.length == 0) return items;
+
+    return items.filter(item => {
+      return item.name.indexOf(term) > -1
+    })
+  }
+
+  onUpdateSearch = (term) => {
+    this.setState({term});
+  }
+
   render() {
+    const {data, term} = this.state;
     const employees = this.state.data.length;
     const increased = this.state.data.filter((item) => item.increase).length;
+    const visibleData = this.searchEmp(data, term);
 
     return (
       <div className="app">
         <AppInfo employees={employees} increased={increased} />
         <div className="search-panel">
-          <SearchPanel />
+          <SearchPanel onUpdateSearch={this.onUpdateSearch}/>
           <AppFilter />
         </div>
         <EmployeesList
-          data={this.state.data}
+          data={visibleData}
           onDelete={this.deleteItem}
           onToggleProp={this.onToggleProp}
         />
